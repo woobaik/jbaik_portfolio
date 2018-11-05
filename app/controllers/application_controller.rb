acf6f906a720124fd7718020e9b73a4c0bfe99c9
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-05 14:06:10 -0800

    Whitelisting custom field for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameter, if: :devise_controller?
+
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
