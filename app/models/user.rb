531aef2a2b436bb7f6ed89c01587fcd64bc5569a
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-06 10:25:04 -0800

    Finish user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+  validates_presence_of :name
   def first_name
     name.split.first
   end
